aea7a17db467ce6b92da8553b209430926ddf129
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-13 10:39:43 +0200

    Square class.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 class square():
+
     width = 0
     height = 0
+
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -18,8 +20,8 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
+
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
